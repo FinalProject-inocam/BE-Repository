0ce77786f3358082be19f9ef57ba1cfc567cf05a
hamonia777 <woong010207@gmail.com>
2023-08-16 03:22:31 +0900

    [feat] : 재방문 의사 샵 상세조회부분에 추가

--- src/main/java/com/example/finalproject/domain/shop/repository/RevisitRepository.java
+++ src/main/java/com/example/finalproject/domain/shop/repository/RevisitRepository.java
@@ -1,11 +1,8 @@
 package com.example.finalproject.domain.shop.repository;
 
-import com.example.finalproject.domain.post.entity.PostLike;
 import com.example.finalproject.domain.shop.entity.Revisit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface RevistRepository extends JpaRepository<Revisit,Long> {
+public interface RevisitRepository extends JpaRepository<Revisit,Long> {
     Revisit findByReviewIdAndUserUserId(Long reviewId, Long userId);
 }
