43bd9c6ef7064fc5afda2f31f05091fa6375d238
kss <kos7114@naver.com>
2023-08-20 01:57:03 +0900

    [feat]: socket message socket.io

--- src/main/java/com/example/finalproject/domain/chat/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.finalproject.domain.chat.repository;
-
-import com.example.finalproject.domain.chat.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
--- src/main/java/com/example/finalproject/domain/sockettest/config/SocketIOConfig.java
+++ src/main/java/com/example/finalproject/domain/sockettest/config/SocketIOConfig.java
@@ -1,39 +1,26 @@
-package com.example.finalproject.domain.chat.config;
+package com.example.finalproject.domain.sockettest.config;
 
 import com.corundumstudio.socketio.SocketIOServer;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Slf4j(topic = "websocket")
 public class SocketIOConfig {
 
-    @Value("${socketio.host}")
+    @Value("${socket-server.host}")
     private String host;
 
-    @Value("${socketio.port}")
+    @Value("${socket-server.port}")
     private Integer port;
 
     @Bean
     public SocketIOServer socketIOServer() {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
-//        config.setHostname(gsdfg);
+        config.setHostname(host);
         config.setPort(port);
-
-        // CORS 설정 추가
-        config.setOrigin("*");
-
-        config.setAuthorizationListener(handshakeData -> {
-            String token = handshakeData.getSingleUrlParam("token");
-            return validateToken(token);
-        });
-
+        //  config.setContext("/socket.io");
         return new SocketIOServer(config);
     }
 
-    private boolean validateToken(String token) {
-        return true;
-    }
 }
