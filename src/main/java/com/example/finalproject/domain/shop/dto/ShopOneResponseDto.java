8ab66b6b6864464910f9d99b0dcd3b28703209c8
hamonia777 <woong010207@gmail.com>
2023-08-22 01:34:22 +0900

    [update] : ShopOneResponseDto에 default 기본 데이터 추가

--- src/main/java/com/example/finalproject/domain/shop/dto/ShopOneResponseDto.java
+++ src/main/java/com/example/finalproject/domain/shop/dto/ShopOneResponseDto.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Getter
@@ -22,6 +23,11 @@ public class ShopOneResponseDto {
     private List<ReviewResponseDto> reviews = new ArrayList<>();
     private List<String> banner;
     private Integer reviewImageSize;
+    private String bussinessHour="08:00~16:00";
+    private String phoneNumber="02-1234-1234";
+    private String bussinessDay="연중무휴";
+    private String detail;
+    private List<String> tag = Arrays.asList("자동차랩핑", "디테일링 세차","디테일링 광택","자동차 튜닝","자동차 썬팅");
 
     public ShopOneResponseDto(JSONObject itemJson, List<Review> reviews,
                               List<ShopLike> shopLikes, User user,
@@ -53,5 +59,6 @@ public class ShopOneResponseDto {
         }
         this.banner=banner;
         this.reviewImageSize = reviewImageSize;
+        this.detail="안녕하세요, 랩핑/PPF 전문 "+itemJson.getString("bizesNm")+"입니다. 랩핑 PPF는 철저한 사후관리가 필수입니다. 맡겨 주시는 만큼 최선을 다해 시공하고, 좋은 품질의 자재와 정확한 기술로 오랜 시간동안 깨끗하고 선명한 상태를 유지하도록 도와드립니다.";
     }
 }
