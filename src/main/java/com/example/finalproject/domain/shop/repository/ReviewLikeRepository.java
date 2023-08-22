acfe8a2a5e2e4cee4eac392bac2364dca46586c5
hamonia777 <woong010207@gmail.com>
2023-08-22 22:33:14 +0900

    [fix] : pagenation 오류 수정

--- src/main/java/com/example/finalproject/domain/shop/repository/ReviewLikeRepository.java
+++ src/main/java/com/example/finalproject/domain/shop/repository/ReviewLikeRepository.java
@@ -3,6 +3,8 @@ package com.example.finalproject.domain.shop.repository;
 import com.example.finalproject.domain.shop.entity.ReviewLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface ReviewLikeRepository extends JpaRepository<ReviewLike,Long> {
@@ -12,4 +14,6 @@ public interface ReviewLikeRepository extends JpaRepository<ReviewLike,Long> {
     Boolean existsByReviewIdAndUserUserId(Long id, Long userId);
 
     Long countByReviewId(Long id);
+
+    List<ReviewLike> findByReviewIdIn(List<Long> reviewIds);
 }
