a4f42a9c4e5fa7c19f62a0a2f45f7d0ef4a0ab06
hamonia777 <woong010207@gmail.com>
2023-08-31 02:59:21 +0900

    [feat] 댓글 페이지네이션 구현

--- src/main/java/com/example/finalproject/domain/post/repository/CommentRepository.java
+++ src/main/java/com/example/finalproject/domain/post/repository/CommentRepository.java
@@ -1,7 +1,14 @@
 package com.example.finalproject.domain.post.repository;
 
 import com.example.finalproject.domain.post.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Page<Comment> findByPostId(Long postId, Pageable pageable);
 }
