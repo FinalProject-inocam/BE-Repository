104c67e7c26dc16377648d5a38fc22411cfa7ac3
진민지 <67037321+realmingji@users.noreply.github.com>
2023-08-31 21:39:17 +0900

    [fix] 닉네임 NotBlank 추가

--- src/main/java/com/example/finalproject/domain/mypage/dto/MypageRequestDto.java
+++ src/main/java/com/example/finalproject/domain/mypage/dto/MypageRequestDto.java
@@ -1,5 +1,7 @@
 package com.example.finalproject.domain.mypage.dto;
 
+import com.example.finalproject.global.validation.ValidationGroups;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,8 +9,12 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class MypageRequestDto {
+    @NotBlank(message = "닉네임은 필수입니다.", groups = ValidationGroups.NotBlankGroup.class)
     String nickname;
+
     String phoneNumber;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#?$%^&*]).{8,15}$", message = "영문자와 숫자, 특수문자를 포함한 8~15자 이내로 작성해주세요")
     String password;
 
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#?$%^&*]).{8,15}$", message = "영문자와 숫자, 특수문자를 포함한 8~15자 이내로 작성해주세요")
